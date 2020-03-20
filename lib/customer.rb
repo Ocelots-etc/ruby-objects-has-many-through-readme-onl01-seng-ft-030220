fe72e8683098eba304f5a1d66008cfac05b7d69c
Cat Gomez <catherinedgomez@gmail.com>
2020-03-20 01:34:24 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -24,10 +24,6 @@ class Customer
     meals.map { |meal| meal.waiter }
   end
 
-  def new_meal(waiter, total, tip=0)
-    Meal.new(waiter, self, total, tip)
-  end
-
   def new_meal_20_percent(waiter, total)
     tip = total * 0.2
     Meal.new(waiter, self, total, tip)
